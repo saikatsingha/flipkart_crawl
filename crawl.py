10021ced3e6d097ad1c52e242ed2414d1d0b2b0a
saikatsingha <saikatsingha@gmail.com>
2021-09-15 03:05:33 +0530

    Mobile data crawling and insertion into DB complete final

--- crawl.py
+++ crawl.py
@@ -26,8 +26,12 @@ while(True):
    driver = webdriver.Firefox()
    driver.get("https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&otracker=categorytree&page="+str(p))
    links = [my_elem.get_attribute("href") for my_elem in driver.find_elements_by_css_selector("a._1fQZEK")]
+   # print("\n\n")
+   # print("\n\n")
    driver.quit()
    for lnk in links :
+      # print(links)
+      # print("\n\n")
       page = requests.get(lnk)
       soup = bs(page.content, "html.parser")
       Name = soup.find("span" , attrs={"class":"B_NuCI"})
@@ -41,9 +45,7 @@ while(True):
       image_url = Image.get("src")
       # Open the url image, set stream to True, this will return the stream content.
       r = requests.get(image_url, stream = True)
-
-      # Check if the image was retrieved successfully
-      
+     
       # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
       r.raw.decode_content = True
       
@@ -55,7 +57,6 @@ while(True):
       k=0
 
       name = Name.get_text().replace(")",'').replace("(",'').replace(",",' ')
-      # print(Price.get_text().replace("₹",'').replace(",",'').strip())
       for k in range(len(Details_Data_Type)):
          if Details_Data_Type[k].get_text() == 'In The Box':
             In_The_Box = Details_Data[k].get_text()
@@ -70,13 +71,13 @@ while(True):
             Processor_Core = Details_Data[k].get_text()
          if Details_Data_Type[k].get_text() == 'Internal Storage':
             # print(Details_Data[k].get_text().replace(' ', '').replace('GB', '').strip())
-            Internal_Storage = Details_Data[k].get_text().replace(' ', '').replace('GB', '').strip()
+            Internal_Storage = Details_Data[k].get_text().replace(' ', '').replace('MB', '').replace('GB', '').strip()
          if Details_Data_Type[k].get_text() == 'RAM':
             # print(Details_Data[k].get_text().replace(' ', '').replace('GB', '').strip())
-            RAM = Details_Data[k].get_text().replace(' ', '').replace('GB', '').strip()
+            RAM = Details_Data[k].get_text().replace(' ', '').replace('GB', '').replace('MB', '').strip()
          if Details_Data_Type[k].get_text() == 'Expandable Storage':
             # print(Details_Data[k].get_text().replace(' ', '').replace('GB', '').strip())
-            expandable_storage = Details_Data[k].get_text().replace('GB', '').strip()
+            expandable_storage = Details_Data[k].get_text().replace('GB', '').replace('MB', '').replace('TB', '').strip()
          if Details_Data_Type[k].get_text() == 'Primary Camera':
             # print(Details_Data[k].get_text())
             Primary_Camera = Details_Data[k].get_text()
@@ -87,39 +88,35 @@ while(True):
             # print(Details_Data[k].get_text())
             Warranty_Summary = Details_Data[k].get_text()
          k=k+1
-
-      # print(Image.get_text())
-      # print(Star.get_text())
-      # print(Rating.get_text())
-      # print(Image.get('src'))
-
             
       cursor = conn.cursor()
-      cursor.execute("""Insert Into mobiles_product_details 
-                     (company_name, price, photo, ram, internal_storage, expandable, display, camera, battery, processor, 
-                     link, warranty, star, rating_review, in_the_box, product_type_id) values
-                     ('"""+str(name)+"""',
-                     """ + Price.get_text().replace("₹",'').replace(",",'').strip()+""",
-                     '"""+image_name+"""', 
-                     """+RAM+""", 
-                     """+Internal_Storage+""", 
-                     """+expandable_storage+""", 
-                     '"""+Display_Size+"""', 
-                     '"""+Primary_Camera+"""', 
-                     """+Battery_Capacity+""",  
-                     '"""+Processor_Type+Processor_Core+"""',  
-                     '"""+s.tinyurl.short(lnk)+"""',
-                     '"""+Warranty_Summary+"""',
-                     """+Star.get_text()+""", 
-                     '"""+Rating.get_text()+"""',
-                     '"""+In_The_Box+"""',
-                     1)""")
-      # sql = "Insert Into mobiles_product_details (company_name, price, photo, ram, rom, expandable, display, camera, battery, processor, link, warranty, star, rating, review, in_the_box, product_type_id) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s, %s,%s,)"
-      # val = ('hdhfhghsg', 9999, 'None', 4, 32, 512, 6.5, '30 mg pxl', 5000, 'mediatech','https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&otracker=categorytree&page=1', '1 years', 4.3, 3200, 170, 'all items', 1)
-      # cursor.execute("""Insert Into mobiles_product_details (company_name, product_type_id) VALUES ('hhjhjh', 1)""")
-      # val = (1'hdhfhghsg', )
-      # cursor.execute(sql, val)
-      conn.commit()
+      for i in range(3):
+         try:
+            cursor.execute("""Insert Into mobiles_product_details 
+                           (company_name, price, photo, ram, internal_storage, expandable, display, camera, battery, processor, 
+                           link, warranty, star, rating_review, in_the_box, product_type_id) values
+                           ('"""+str(name)+"""',
+                           """ + Price.get_text().replace("₹",'').replace(",",'').strip()+""",
+                           '"""+image_name+"""', 
+                           """+RAM+""", 
+                           """+Internal_Storage+""", 
+                           """+expandable_storage+""", 
+                           '"""+Display_Size+"""', 
+                           '"""+Primary_Camera+"""', 
+                           """+Battery_Capacity+""",  
+                           '"""+Processor_Type+Processor_Core+"""',  
+                           '"""+s.tinyurl.short(lnk)+"""',
+                           '"""+Warranty_Summary+"""',
+                           """+Star.get_text()+""", 
+                           '"""+Rating.get_text()+"""',
+                           '"""+In_The_Box+"""',
+                           1) ON CONFLICT (company_name) DO NOTHING""")
+
+         except Exception as e:
+            conn.rollback()
+         else:
+            conn.commit()
+  
       cursor.close()
       
    p=p+1
