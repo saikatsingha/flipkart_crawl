dc0c4e82206bb974560d87e5d94ea778d9411a9c
saikatsingha <saikatsingha@gmail.com>
2021-09-24 21:21:17 +0530

    Just started react

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h3 className="m-3 d-flex justify-content-center">
+        Saikat Singha
+      </h3>
+      
+      
+    </div>
+  );
+}
+
+export default App;
